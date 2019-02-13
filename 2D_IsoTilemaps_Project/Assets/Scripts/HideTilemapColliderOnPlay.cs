7a660f5186b0ee51c1f192177ea72f91982dcb5c
Andy Touch <andyt@unity3d.com>
2019-02-13 18:12:00 +0100

    Added Project (Unity 2018.3.0f2)
    Packages:
    2D Pixel Perfect: 1.0.1-preview

---
+++ 2D_IsoTilemaps_Project/Assets/Scripts/HideTilemapColliderOnPlay.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideTilemapColliderOnPlay : MonoBehaviour
+{
+
+    private TilemapRenderer tilemapRenderer;
+
+    void Start()
+    {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+        tilemapRenderer.enabled = false;
+    }
+}
